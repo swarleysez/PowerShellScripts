c8cefc0005511dd3122f9cb138beef9a46efc905
swarleysez <31361532+swarleysez@users.noreply.github.com>
2018-05-13 01:41:37 -0400

    Update set_static_dns_localhost.ps1

--- set_static_dns_localhost.ps1
+++ set_static_dns_localhost.ps1
@@ -1,14 +1,10 @@
-#$NICs = Get-WMIObject Win32_NetworkAdapterConfiguration -computername . | where{$_.IPEnabled -eq $true -and $_.DHCPEnabled -eq $true}
 $NICs = Get-WMIObject Win32_NetworkAdapterConfiguration -computername . | where{$_.IPEnabled -eq $true}
-Foreach($NIC in $NICs) {
-#$ip = ($NIC.IPAddress[0])
-#$gateway = $NIC.DefaultIPGateway
-#$subnet = $NIC.IPSubnet[0]
-#$dns = $NIC.DNSServerSearchOrder
-$dns = "172.16.1.100" , "172.16.1.101"
-#$NIC.EnableStatic($ip, $subnet)
-#$NIC.SetGateways($gateway)
-$NIC.SetDNSServerSearchOrder($dns)
-$NIC.SetDynamicDNSRegistration(“FALSE”)
+
+Foreach ($NIC in $NICs)
+{
+  $dns = "172.16.1.100" , "172.16.1.101"
+  $NIC.SetDNSServerSearchOrder($dns)
+  $NIC.SetDynamicDNSRegistration(“FALSE”)
 }
+
 IPConfig /all
